20761f16098e1d4e64808b07954819bafbfb13b9
Karacky <mrnk3078@gmail.com>
2021-11-27 18:51:50 +0900

    add isu_condition_jia_isu_uuid_idx

--- sql/0_Schema.sql
+++ sql/0_Schema.sql
@@ -1,6 +1,9 @@
 DROP TABLE IF EXISTS `isu_association_config`;
+
 DROP TABLE IF EXISTS `isu_condition`;
+
 DROP TABLE IF EXISTS `isu`;
+
 DROP TABLE IF EXISTS `user`;
 
 CREATE TABLE `isu` (
@@ -12,8 +15,8 @@ CREATE TABLE `isu` (
   `jia_user_id` VARCHAR(255) NOT NULL,
   `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
   `updated_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
-   PRIMARY KEY(`id`)
-) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
+  PRIMARY KEY(`id`)
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4;
 
 CREATE TABLE `isu_condition` (
   `id` bigint AUTO_INCREMENT,
@@ -23,15 +26,16 @@ CREATE TABLE `isu_condition` (
   `condition` VARCHAR(255) NOT NULL,
   `message` VARCHAR(255) NOT NULL,
   `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
-  PRIMARY KEY(`id`)
-) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
+  PRIMARY KEY(`id`),
+  INDEX isu_condition_jia_isu_uuid_idx(`jia_isu_uuid`)
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4;
 
 CREATE TABLE `user` (
   `jia_user_id` VARCHAR(255) PRIMARY KEY,
   `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6)
-) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4;
 
 CREATE TABLE `isu_association_config` (
   `name` VARCHAR(255) PRIMARY KEY,
   `url` VARCHAR(255) NOT NULL UNIQUE
-) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4;
