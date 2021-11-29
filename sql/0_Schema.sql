65ae465b685388465a968feb92729b3fdd02d0b6
Karacky <mrnk3078@gmail.com>
2021-11-29 20:27:37 +0900

    jia_user_id

--- sql/0_Schema.sql
+++ sql/0_Schema.sql
@@ -33,7 +33,7 @@ CREATE TABLE `isu_condition` (
 ) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4;
 
 CREATE TABLE `user` (
-  `jia_user_id` VARCHAR(255) PRIMARY KEY,
+  `jia_user_id` VARCHAR(30) PRIMARY KEY,
   `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6)
 ) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4;
 
