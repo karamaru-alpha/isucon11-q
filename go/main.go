10fe7367789ff01e1d484da4430c0beae9535087
Karacky <mrnk3078@gmail.com>
2021-12-01 21:27:18 +0900

    trend

--- go/main.go
+++ go/main.go
@@ -202,7 +202,7 @@ func (o *omTrendResT) Get() ([]TrendResponse, bool) {
 
 func (o *omTrendResT) Set(v []TrendResponse) {
 	o.M.Lock()
-	o.T = time.Now().Add(time.Second)
+	o.T = time.Now().Add(time.Second * 5)
 	o.V = v
 	o.M.Unlock()
 }
