7a22ee7ba03f868226806a57f4750863f5ca45ce
Karacky <mrnk3078@gmail.com>
2021-12-25 17:03:45 +0900

    x-accel-redirect

--- go/main.go
+++ go/main.go
@@ -391,8 +391,7 @@ func main() {
 		e.Logger.Fatalf("failed to connect db: %v", err)
 		return
 	}
-	db.SetMaxOpenConns(1024)
-	db.SetMaxIdleConns(1024)
+	db.SetMaxOpenConns(20)
 	defer db.Close()
 
 	postIsuConditionTargetBaseURL = os.Getenv("POST_ISUCONDITION_TARGET_BASE_URL")
